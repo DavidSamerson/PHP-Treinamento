12b9dc93ea106e9b2e659df98fff920bcfd2f6b6
DavidSamerson <samerson10@gmail.com>
2017-08-17 14:49:46 -0300

    nova aba de cadastro

--- Sistema Tabelas e LOGIN/pagina/tabela.php
+++ Sistema Tabelas e LOGIN/pagina/tabela.php
@@ -10,37 +10,88 @@
           <h3>Table</h3>
           <br>
         </div> <!-- /widget-header -->
-        
-        <div class="widget-content">
+        <div class="widget-content col-sm-8">
           
           <table class="table table-striped table-bordered">
             <thead>
               <tr>
-                <th>Engine</th>
-                <th>Browser</th>
-                <th class="td-actions"></th>
+                <th>NOME</th>
+                <th>EMAIL</th>
+                <th class="td-actions"> EDITAR/DELETAR</th>
               </tr>
             </thead>
             <tbody>
+
+            <?php
+
+              include("conexao.php");
+
+              $sqlListar = "SELECT * FROM pessoas";
+              $query = $mysqli->query($sqlListar);
+              
+              while ($resultado = $query->fetch_array(MYSQL_BOTH)) {
+                
+                print "
+
               <tr>
-                <td>Trident</td>
-                <td>Internet
-                   Explorer 4.0</td>
-                <td class="td-actions">
-                  <a href="javascript:;" class="btn btn-small btn-primary">
-                    <i class="btn-icon-only icon-ok"></i>                   
+                <td>$resultado[nome]</td>
+                <td>$resultado[email]</td>
+
+               <td class='td-actions'>
+                  <a href='index.php?id=$resultado[id]' class='btn btn-small btn-primary'>
+                    <i class='btn-icon-only icon-ok'>EDITAR</i>                   
                   </a>
                   
-                  <a href="javascript:;" class="btn btn-small">
-                    <i class="btn-icon-only icon-remove"></i>                   
+                  <a href='remover.php?id=$resultado[id]' class='btn btn-small'>
+                    <i class='btn-icon-only icon-remove'>
+                    DELETAR</i>                   
                   </a>
                 </td>
               </tr>
+
+                  ";
+
+              }
+
+            ?>
               
               </tbody>
             </table>
           
         </div> <!-- /widget-content -->
-      
+
+        <div class="col-sm-4" align="center">
+         
+         <legend>CADASTRO</legend>
+
+          <form action="" method="">
+            
+          <div class="form-group" style="margin-bottom: 2%">
+            <label class="col-sm-6 control-label" for="nome">
+              NOME
+            </label>
+            <div class="col-sm-6">
+              <input type="text" name="nome" id="nome" placeholder="Nome da Pessoa" class="form-control input-md">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-sm-6 control-label" for="email">
+              EMAIL
+            </label>
+            <div class="col-sm-6">
+              <input type="email" name="email" id="email" placeholder="Nome da Pessoa" class="form-control input-md">
+            </div>
+          </div>
+          <div class="form-group col-sm-4 col-sm-offset-8">
+          <br>
+          <button type="submit" class="bnt btn-primary bnt-block">CADASTRAR</button>
+            </div>
+          </form>
+      </div>
+
+
+
+
+
       </div> <!-- /widget -->
             </div>
