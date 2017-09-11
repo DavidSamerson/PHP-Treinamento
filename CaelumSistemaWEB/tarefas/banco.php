58289cf0b964458cf7863c0a2a6a771b6c291de9
David Samerson <samerson10@gmail.com>
2017-09-11 15:06:52 -0300

    novo

--- CaelumSistemaWEB/tarefas/banco.php
+++ CaelumSistemaWEB/tarefas/banco.php
@@ -1,15 +1,39 @@
 <?php
 
-$bdServidor = '127.0.0.1';
-$bdUsuario = 'sistematarefa';
-$bdSenha = 'sistema';
-$bdBanco = 'tarefas';
-
-$conexao = mysqli_connect($bdServidor, $bdUsuario, $bdSenha, $bdBanco);
+$conexao = mysqli_connect(BD_SERVIDOR, BD_USUARIO, BD_SENHA, BD_BANCO);
 
 if (mysqli_connect_errno($conexao)) {
     echo "Problemas para conectar no banco. Verifique os dados!";
     die();
+}
+
+function gravar_anexo($conexao, $anexo)
+{
+    $sqlGravar = "INSERT INTO anexos
+        (tarefa_id, nome, arquivo)
+        VALUES
+        (
+            {$anexo['tarefa_id']},
+            '{$anexo['nome']}',
+            '{$anexo['arquivo']}'
+        )
+    ";
+
+    mysqli_query($conexao, $sqlGravar);
+}
+
+function buscar_anexos($conexao, $tarefa_id)
+{
+    $sqlBusca = "SELECT * FROM anexos WHERE tarefa_id = {$tarefa_id}";
+    $resultado = mysqli_query($conexao, $sqlBusca);
+
+    $anexos = array();
+
+    while ($anexo = mysqli_fetch_assoc($resultado)) {
+        $anexos[] = $anexo;
+    }
+
+    return $anexos;
 }
 
 function buscar_tarefas($conexao)
--- CaelumSistemaWEB/tarefas/formulario.php
+++ CaelumSistemaWEB/tarefas/formulario.php
@@ -1,9 +1,12 @@
-<form>
+<form method="POST">
     <input type="hidden" name="id" value="<?php echo $tarefa['id']; ?>" />
     <fieldset>
         <legend>Nova tarefa</legend>
         <label>
             Tarefa:
+            <?php if ($tem_erros && isset($erros_validacao['nome'])) : ?>
+                <span class="erro"><?php echo $erros_validacao['nome']; ?></span>
+            <?php endif; ?>
             <input type="text" name="nome" value="<?php echo $tarefa['nome']; ?>" />
         </label>
         <label>
@@ -12,6 +15,9 @@
         </label>
         <label>
             Prazo (Opcional):
+            <?php if ($tem_erros && isset($erros_validacao['prazo'])) : ?>
+                <span class="erro"><?php echo $erros_validacao['prazo']; ?></span>
+            <?php endif; ?>
             <input type="text" name="prazo" value="<?php echo traduz_data_para_exibir($tarefa['prazo']); ?>" />
         </label>
         <fieldset>
@@ -23,6 +29,10 @@
         <label>
             Tarefa concluída:
             <input type="checkbox" name="concluida" value="1" <?php echo ($tarefa['concluida'] == 1) ? 'checked' : ''; ?> />
+        </label>
+        <label>
+            Lembrete por e-mail:
+            <input type="checkbox" name="lembrete" value="1" />
         </label>
         <input type="submit" value="<?php echo ($tarefa['id'] > 0) ? 'Atualizar' : 'Cadastrar'; ?>" />
     </fieldset>
