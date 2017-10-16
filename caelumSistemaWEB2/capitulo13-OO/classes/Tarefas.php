a98403d94265dcb893b8586d4a9b08977df8caea
David Samerson <samerson10@gmail.com>
2017-10-16 17:39:03 -0300

    novo

---
+++ caelumSistemaWEB2/capitulo13-OO/classes/Tarefas.php
@@ -0,0 +1,34 @@
+<?php 
+
+	/**
+	* Arquivo: Classes/Tarefas.php
+	*/
+	class Tarefas
+	{
+		public $conexao;
+		public $tarefas = array();
+
+
+		public function __construct($nova_conexao)
+		{
+			$this->conexao = $nova_conexao;
+		}
+
+		public function buscar_tarefas()
+		{
+			$sqlBusca - 'SELECT * FROM tarefas';
+			$resultado = mysqli_query($this->conexao, $sqlBusca);
+
+			$this->tarefas = array();
+
+		}
+
+		while ($tarefa = mysqli_fetch_assoc($resultado)) {
+
+			$this->tarefas[] = $tarefa;
+			
+		}
+	}
+
+
+ ?>
